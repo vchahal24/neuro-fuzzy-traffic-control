0771e80a49a7f44ae50ae25c7a90bb3a9777fcc6
Vikram Chahal <vikramchahal24@protonmail.com>
2026-03-05 23:35:36 -0500

    Fixed simulation package naming

--- src/sim/__init__.py
+++ src/sim/__init__.py
@@ -1,15 +1,15 @@
 # SIMULATION PACKAGE
 # Purpose: This re exports the pieces so they can be imported cleanly
 
-# simulationconfigurationModels
+# simConfigModels
 
-from .simulationConfigurationModels import ControllerConfig, DataSelectionConfig, SimulationConfig
+from .SimConfigModels import ControllerConfig, DataSelectionConfig, SimulationConfig
 
 # trafficSignalControllerModels
 from .trafficSignalControllerModels import build_controller
 
 # Loads your three preprocessed CSV tables into one per intersection object used by simulation:
-from .intersectionTrafficDataLoader import load_selected_intersections
+from .intersectionDataLoader import loadIntersectionCall
 
 # Runs the traffic experiments:
 from .trafficExperimentRunner import run_experiments
@@ -20,6 +20,6 @@ __all__ = [
     "DataSelectionConfig",
     "SimulationConfig",
     "build_controller",
-    "load_selected_intersections",
+    "loadIntersectionCall",
     "run_experiments",
 ]
